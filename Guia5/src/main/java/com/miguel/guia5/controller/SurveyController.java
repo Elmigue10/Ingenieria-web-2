304e927984fffbb0898598603175a445e462758d
Elmigue10 <miguelvalbuena1026@gmail.com>
2022-04-25 11:56:58 -0500

    Guia 5 - Encuesta con Spring Boot, JSP, MySQL y Cookies

--- Guia5/src/main/java/com/miguel/guia5/controller/SurveyController.java
+++ Guia5/src/main/java/com/miguel/guia5/controller/SurveyController.java
@@ -3,7 +3,10 @@ package com.miguel.guia5.controller;
 import com.miguel.guia5.model.Superhero;
 import com.miguel.guia5.model.SuperheroDTO;
 import com.miguel.guia5.service.SuperheroService;
+import com.mysql.cj.log.Log;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -22,14 +25,17 @@ public class SurveyController {
 
     private final SuperheroService superheroService;
     private Superhero superhero;
+    private static final Logger logger = LogManager.getLogger("SuperheroesSurveyController");
 
     @GetMapping("/")
     public String index(Model model,
                         @CookieValue(value = "superheroesSurvey.superheroName", defaultValue = "notCookie") String superheroName){
 
+        logger.info("Obteniendo todos los superheroes de la base de datos");
         List<Superhero> superheroes = superheroService.listar();
 
         if(!superheroName.equals("notCookie")){
+            logger.info("Leyendo cookies del usuario");
             model.addAttribute("superheroName", superheroName);
             model.addAttribute("messageCookie", "Tu anterior voto fue para: ");
         }
@@ -47,33 +53,38 @@ public class SurveyController {
                         throws UnsupportedEncodingException {
 
         if(!superheroName.equals("notCookie")){
+            logger.info("Leyendo cookies del usuario");
             model.addAttribute("superheroName", superheroName);
             model.addAttribute("messageCookie", "Tu anterior voto fue para: ");
         }
 
         if(!id.equals("notId")) {
+            logger.info("Actualizando votos para el superheroe con el id: " + id);
             superhero = superheroService.actualizarVoto(Integer.valueOf(id));
             model.addAttribute("mensaje", "Has votado por: ");
             model.addAttribute("superheroVotado", superhero);
+
+            //      Creando las cookies
+            logger.info("Creando cookies del usuario");
+            Cookie cookieSuperheroId = new Cookie("superheroesSurvey.superheroId",
+                    URLEncoder.encode( String.valueOf(superhero.getId()), "UTF-8" ).replace("+", "%20"));
+            Cookie cookieSuperheroName = new Cookie("superheroesSurvey.superheroName",
+                    URLEncoder.encode( superhero.getNombre(), "UTF-8" ).replace("+", "%20"));
+            Cookie cookieSuperheroVotos = new Cookie("superheroesSurvey.superheroVotos",
+                    URLEncoder.encode( String.valueOf(superhero.getId()), "UTF-8" ).replace("+", "%20"));
+            //      Vida de la cookie
+            cookieSuperheroId.setMaxAge(365 * 24 * 60 * 60); //Un año de vida para la cookie
+            cookieSuperheroName.setMaxAge(365 * 24 * 60 * 60); //Un año de vida para la cookie
+            cookieSuperheroVotos.setMaxAge(365 * 24 * 60 * 60); //Un año de vida para la cookie
+            //      Eviar las cookies en la respuesta
+            response.addCookie(cookieSuperheroId);
+            response.addCookie(cookieSuperheroName);
+            response.addCookie(cookieSuperheroVotos);
         }
 
-//      Creando las cookies
-        Cookie cookieSuperheroId = new Cookie("superheroesSurvey.superheroId",
-                URLEncoder.encode( String.valueOf(superhero.getId()), "UTF-8" ).replace("+", "%20"));
-        Cookie cookieSuperheroName = new Cookie("superheroesSurvey.superheroName",
-                URLEncoder.encode( superhero.getNombre(), "UTF-8" ).replace("+", "%20"));
-        Cookie cookieSuperheroVotos = new Cookie("superheroesSurvey.superheroVotos",
-                URLEncoder.encode( String.valueOf(superhero.getId()), "UTF-8" ).replace("+", "%20"));
-//      Vida de la cookie
-        cookieSuperheroId.setMaxAge(365 * 24 * 60 * 60); //Un año de vida para la cookie
-        cookieSuperheroName.setMaxAge(365 * 24 * 60 * 60); //Un año de vida para la cookie
-        cookieSuperheroVotos.setMaxAge(365 * 24 * 60 * 60); //Un año de vida para la cookie
-//      Eviar las cookies en la respuesta
-        response.addCookie(cookieSuperheroId);
-        response.addCookie(cookieSuperheroName);
-        response.addCookie(cookieSuperheroVotos);
-
+        logger.info("Obteniendo los votos totales de los superheroes");
         int votosTotales = superheroService.votosTotales();
+        logger.info("Obteniendo porcentaje de votos de cada supeheroe");
         List<SuperheroDTO> superheroes = superheroService.superheroesPercentage();
         model.addAttribute("superheroes", superheroes);
         model.addAttribute("votosTotales", votosTotales);
--- Guia5/src/main/java/com/miguel/guia5/service/SuperheroService.java
+++ Guia5/src/main/java/com/miguel/guia5/service/SuperheroService.java
@@ -4,6 +4,8 @@ import com.miguel.guia5.model.Superhero;
 import com.miguel.guia5.model.SuperheroDTO;
 import com.miguel.guia5.repository.SuperheroRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.text.DecimalFormat;
@@ -18,13 +20,16 @@ public class SuperheroService {
     private final SuperheroRepository superheroRepository;
     private Superhero superhero;
     private List<Superhero> superperheroes;
+    private static final Logger logger = LogManager.getLogger("SuperheroesSurveyService");
 
     public List<Superhero> listar(){
+        logger.info("Obteniendo superheroes de la base de datos");
         superperheroes = (List<Superhero>) superheroRepository.findAll();
         return superperheroes;
     }
 
     public Superhero actualizarVoto(Integer id){
+        logger.info("Actualizando votos del superheroe con id: " + id);
         superhero = superheroRepository.findById(id).get();
 
         superhero.setVotos(superhero.getVotos()+1);
@@ -35,6 +40,7 @@ public class SuperheroService {
     }
 
     public int votosTotales(){
+        logger.info("Obteniendo la sumatoria de los votos totales");
         superperheroes = (List<Superhero>) superheroRepository.findAll();
         int votosTotales = 0;
         for (Superhero superheroIter: superperheroes) {
@@ -44,6 +50,7 @@ public class SuperheroService {
     }
 
     public List<SuperheroDTO> superheroesPercentage(){
+        logger.info("Obteniendo el porcentaje de votos de cada superheroe");
         DecimalFormat df = new DecimalFormat("#.00");
         List<SuperheroDTO> superheroDTOS = new ArrayList<>();
         superperheroes = (List<Superhero>) superheroRepository.findAll();
